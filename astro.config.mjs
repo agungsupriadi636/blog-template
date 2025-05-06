f7b362d48a9d989ff190090b6c9690a8d22e654e
agungsupriadi636 <agungsupriadi636@gmail.com>
2025-05-06 14:55:46 +0000

    update astro dan pkgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 import remarkToc from 'remark-toc';
 import { remarkReadingTime } from './src/plugin/reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [ tailwind(), mdx(), sitemap()],
-	markdown: {
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx(), sitemap()],
+  markdown: {
     // Applied to .md and .mdx files
-    remarkPlugins: [ remarkReadingTime, [remarkToc, { heading: 'toc', maxDepth: 3 }]],
+    remarkPlugins: [remarkReadingTime, [remarkToc, { heading: 'toc', maxDepth: 3 }]],
   },
 });
