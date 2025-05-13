8a8db6cbde4e6fe8964416aca727746e864904fc
agungsupriadi636 <agungsupriadi636@gmail.com>
2025-05-13 16:50:53 +0000

    perbaiki file config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://blog-anda.com',
   experimental: {
     fonts: [{
       provider: fontProviders.google(),
